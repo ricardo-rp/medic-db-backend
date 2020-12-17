95b07b49fad3c7c66aa58304b5baf9d2ff1201ec
Moro <ricardo.romero@gmail.com>
2020-12-17 16:11:25 -0300

    updated DB

---
+++ app/data/schema/patient-schema.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS Patients (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    full_name VARCHAR,
+    birth_date DATETIME,
+    mother_name VARCHAR,
+    city VARCHAR,
+    handbook_number INTEGER,
+    bed_number INTEGER,
+    sex VARCHAR,
+    status_id INTEGER,
+    surgery_id INTEGER,
+    weight FLOAT,
+    FOREIGN KEY(status_id) REFERENCES Status(id),
+    FOREIGN KEY(surgery_id) REFERENCES Surgeries(id)
+);
--- app/data/setup-database.js
+++ app/data/setup-database.js
@@ -1,10 +1,18 @@
-var fs = require('fs');
-var sqlSchema = fs.readFileSync('app/data/product-schema.sql').toString();
+var fs = require("fs");
+var patientSchema = fs
+  .readFileSync("app/data/schema/patient-schema.sql")
+  .toString();
+var statusSchema = fs
+  .readFileSync("app/data/schema/status-schema.sql")
+  .toString();
+var surgerySchema = fs
+  .readFileSync("app/data/schema/surgery-schema.sql")
+  .toString();
 
-module.exports = function(db) {
-    db.serialize(function() {
-        db.run(sqlSchema);
-    });
+module.exports = function (db) {
+  db.serialize(function () {
+    db.run(patientSchema);
+    db.run(statusSchema);
+    db.run(surgerySchema);
+  });
 };
-
-
