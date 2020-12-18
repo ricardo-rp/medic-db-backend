652c03a8364e6646c6f7972789e2237bea748fc8
Moro <ricardo.romero@gmail.com>
2020-12-17 23:56:18 -0300

    enabled cors

--- server.js
+++ server.js
@@ -5,9 +5,13 @@ const port = 4300;
 var sqlite3 = require('sqlite3');
 var db = new sqlite3.Database('app/data/sqlitedb');
 var bodyParser = require("body-parser");
+var cors = require('cors');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+
 
 require('./app/routes')(app, db);
 
